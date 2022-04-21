dbe5b2051b395d4e01f56963c21ddc55aae1374e
Seagull <103934114+ItsTheWhale@users.noreply.github.com>
2022-04-21 12:23:38 +0800

    cookies

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 const cache = {
-    setCookie: function(name, value, expiry) {
-        let placeholder = expiry;
-        document.cookie = `${name}=${value}; expiry=${placeholder}`
+    setCookie: function(name, value, expiryDays, path) {
+        const d = new Date();
+        d.setTime(d.getTime() + (expiryDays*24*60*60*1000));
+        let expiry = "expires="+ d.toUTCString();
+        document.cookie = `${name}=${value}; expiry=${expiry}; path=${path}`
+    },
+    getCookie: function(cname) {
+        let name = cname + "=";
+        let decodedCookie = decodeURIComponent(document.cookie);
+        let ca = decodedCookie.split(';');
+        for(let i = 0; i < ca.length; i++) {
+            let c = ca[i];
+            while (c.charAt(0) == ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) == 0) {
+                return c.substring(name.length, c.length);
+            }
+        }
+        return "";
+    },
+    deleteCookie: function(name) {
+        document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/`;
     }
-}
+};
