3d0b122d89e6b85a832e3d5dafc27171a99eb585
Seagull <103934114+ItsTheWhale@users.noreply.github.com>
2022-04-25 19:16:32 +0800

    Fixed 404 theme

---
+++ 404.js
@@ -0,0 +1,36 @@
+const cache = {
+    setCookie: function (name, value, expiryDays, path) {
+        const d = new Date();
+        d.setTime(d.getTime() + (expiryDays * 24 * 60 * 60 * 1000));
+        let expiry = d.toUTCString();
+        document.cookie = `${name}=${value}; expires=${expiry}; path=${path}`
+    },
+    getCookie: function (cname) {
+        let name = cname + "=";
+        let decodedCookie = decodeURIComponent(document.cookie);
+        let ca = decodedCookie.split(';');
+        for (let i = 0; i < ca.length; i++) {
+            let c = ca[i];
+            while (c.charAt(0) == ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) == 0) {
+                return c.substring(name.length, c.length);
+            }
+        }
+        return "";
+    },
+    deleteCookie: function (name) {
+        document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/`;
+    }
+};
+let settings = {
+    theme: "light",
+    themeNumber: 0
+};
+const initialisePage = {
+    theme: function() {
+        settings.theme = cache.getCookie("theme");
+        document.getElementsByTagName("html")[0].setAttribute("class", settings.theme);
+    }
+};
