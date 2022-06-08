ed4a39bdde64ac9f0dc63b1332def6a9220f8bda
ItsTheWhale <103934114+ItsTheWhale@users.noreply.github.com>
2022-06-08 09:22:30 +0800

    Made the word counter a single file

--- word-counter/script.js
+++
@@ -1,8 +0,0 @@
-let text = '';
-let textarea = document.getElementById("text");
-textarea.addEventListener("change", () => {
-    text = textarea.value;
-    document.getElementById("charactercount").innerText = text.split('').length;
-    document.getElementById("wordcount").innerText = text.split(' ').length;
-    document.getElementById("sentencecount").innerText = text.split('.').length;
-})
