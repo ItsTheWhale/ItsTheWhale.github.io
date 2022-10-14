3d8f967d4a39183a9beebc68b359060d828fed88
ItsTheWHale <103934114+ItsTheWhale@users.noreply.github.com>
2022-10-14 18:52:36 +0800

    update to include metric time

--- utils/hover.js
+++ utils/hover.js
@@ -1,7 +1,7 @@
 const projectDescription = {
-    projectDesc: ["A simple CPS test", "Find the average, median and mode", "A simple word counter"],
-    projectId: ["project-clickTest", "project-autoMMM", "project-wordCount"],
-    descId: ["projectdesc-clickTest", "projectdesc-autoMMM", "projectdesc-wordCount"]
+    projectDesc: ["A simple CPS test", "Find the average, median and mode", "A simple word counter", "Calculates Metric Time"],
+    projectId: ["project-clickTest", "project-autoMMM", "project-wordCount", "project-metric-time"],
+    descId: ["projectdesc-clickTest", "projectdesc-autoMMM", "projectdesc-wordCount", "projectdesc-metric-time"]
 };
 for (let i in projectDescription.projectId) {
     let project = document.getElementById(projectDescription.projectId[i]);
